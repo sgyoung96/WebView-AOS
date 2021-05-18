a93e2c5170d7c13ab2387fdda37d22e62502bb7e
송가영 <sgyoung96@naver.com>
2021-05-18 19:57:37 +0900

    WebViewInterface 파일 추가, 1차 소스 완성

--- WebVeiewToClient/app/src/main/java/com/example/webviewtoclient/MainActivity.kt
+++ WebVeiewToClient/app/src/main/java/com/example/webviewtoclient/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.webviewtoclient
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+    lateinit var tvText: TextView
+    lateinit var btnClick: Button
+    lateinit var etText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.wv_header)
+        tvText = findViewById(R.id.tv_text)
+        btnClick = findViewById(R.id.btn_click)
+        etText = findViewById(R.id.et_text)
+
+        val websetting = webView.settings
+        websetting.javaScriptEnabled = true
+
+        webView.addJavascriptInterface(WebViewInterface(this, webView), "Interface")
+        webView.loadUrl("file:///android_asset/sample.html")
+
+        btnClick.setOnClickListener { webView.loadUrl("javascript:exam_script.plus_num(" + etText.text + ")") }
     }
 }
---
+++ WebVeiewToClient/app/src/main/java/com/example/webviewtoclient/WebViewInterface.kt
@@ -0,0 +1,16 @@
+package com.example.webviewtoclient
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+
+class WebViewInterface constructor(val context: Context, val webView: WebView) {
+
+    val main = MainActivity()
+
+    @JavascriptInterface
+    fun getDoubleNum(result: Int) {
+        main.tvText.text = result as String
+    }
+
+}
